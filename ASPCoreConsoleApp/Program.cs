c15d543939c8c75b6f908e562ed82938d6c5c6b0
Mohsin Nazir <mohsin2851@outlook.com>
2019-04-25 03:06:57 +0500

    Add project files.

---
+++ ASPCoreConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASPCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
